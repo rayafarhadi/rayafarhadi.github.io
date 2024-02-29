e597123ea8573d26306536e6a49f9ea05ba68184
Raya Farhadi <raya.farhadi@gmail.com>
2024-02-29 16:25:08 -0500

    added some colour

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -7,18 +7,20 @@ const HeroSection = () => {
     <div className="grid grid-cols-12">
       <div className="col-span-7 place-self-center">
         <h1 className="text-white mb-4 text-6xl font-extrabold">
-          {" "}
-          Hello I'm Raya
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-teal-300 to-blue-700">
+            Hello I'm{" "}
+          </span>
+          Raya Farhadi
         </h1>
         <p className="text-[#adb7be] mb-6 text-xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam velit
           turpis, porta in dignissim eu, blandit in velit.
         </p>
-        <button className="px-6 py3 w-fit rounded-full mr-4 bg-white text-black">
+        <button className="px-6 py-3 w-fit rounded-full mr-4 bg-gradient-to-br from-teal-300 to-blue-700 text-white">
           Hire Me
         </button>
-        <button className="px-6 py3 w-fit rounded-full bg-white text-black">
-          Download Resume
+        <button className="px-1 py-1 w-fit rounded-full bg-gradient-to-br from-teal-300 to-blue-700 text-white">
+          <span className="px-5 py-2 block bg-[#121212] hover:bg-slate-800 rounded-full">Download Resume</span>
         </button>
       </div>
       <div className="col-span-5 place-self-center">
