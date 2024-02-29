aba08801ee2f12192f0218a42cb4b1be0abb4895
Raya Farhadi <raya.farhadi@gmail.com>
2024-02-29 16:36:45 -0500

    added smaller screen support

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -6,9 +6,9 @@ import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
-    <div className="grid grid-cols-12">
-      <div className="col-span-7 place-self-center">
-        <h1 className="text-white mb-4 text-6xl font-extrabold">
+    <div className="grid grid-cols-1 lg:grid-cols-12">
+      <div className="col-span-7 place-self-center text-center sm:text-left">
+        <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-teal-300 to-blue-700">
             Hello I'm{" "}
           </span>
@@ -28,19 +28,19 @@ const HeroSection = () => {
             repeat={Infinity}
           />
         </h1>
-        <p className="text-[#adb7be] mb-6 text-xl">
+        <p className="text-[#adb7be] mb-6 text-base sm:text-large lg:text-xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam velit
           turpis, porta in dignissim eu, blandit in velit.
         </p>
-        <button className="px-6 py-3 w-fit rounded-full mr-4 bg-gradient-to-br from-teal-300 to-blue-700 text-white">
+        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-teal-300 to-blue-700 text-white">
           Hire Me
         </button>
-        <button className="px-1 py-1 w-fit rounded-full bg-gradient-to-br from-teal-300 to-blue-700 text-white">
+        <button className="px-1 py-1 w-full sm:w-fit rounded-full mt-3 sm:mt-0 bg-gradient-to-br from-teal-300 to-blue-700 text-white">
           <span className="px-5 py-2 block bg-[#121212] hover:bg-slate-800 rounded-full">Download Resume</span>
         </button>
       </div>
-      <div className="col-span-5 place-self-center">
-        <div className="rounded-full bg-[#181818] w-[350px] h-[350px] relative">
+      <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+        <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[350px] lg:h-[350px] relative">
           <Image
             src="/images/hero-image.png"
             alt="hero image"
