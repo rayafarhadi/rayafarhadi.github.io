b38ab282d121ea76cb7650e2a9e65683b537e81b
Raya Farhadi <raya.farhadi@gmail.com>
2024-03-01 20:26:29 -0500

    added more mobile friendly typing animation

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -12,20 +12,37 @@ const HeroSection = () => {
           <span className="text-transparent bg-clip-text bg-gradient-to-r theme-gradient">
             Hello I'm{" "}
           </span>
-          <br/>
-          <TypeAnimation 
-            sequence={[
-              "Raya Farhadi",
-              1000,
-              "A Web Developer",
-              1000,
-              "A Mobile Developer",
-              1000
-            ]}
-            wrapper="span"
-            speed={50}
-            repeat={Infinity}
-          />
+          <br />
+          <div className="hidden sm:block">
+            <TypeAnimation
+              sequence={[
+                "Raya Farhadi",
+                1000,
+                "A Web Developer",
+                1000,
+                "A Mobile Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </div>
+          <div className="sm:hidden">
+            <TypeAnimation
+              sequence={[
+                "Raya Farhadi",
+                1000,
+                "A Web Dev",
+                1000,
+                "A Mobile Dev",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </div>
         </h1>
         <p className="text-text-secondary mb-6 text-base sm:text-large lg:text-xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam velit
@@ -35,7 +52,9 @@ const HeroSection = () => {
           Contact Me
         </button>
         <button className="px-1 py-1 w-full sm:w-fit rounded-full mt-3 sm:mt-0 bg-gradient-to-br theme-gradient text-white">
-          <span className="px-5 py-2 block bg-background-primary hover:bg-slate-800 rounded-full">Download Resume</span>
+          <span className="px-5 py-2 block bg-background-primary hover:bg-slate-800 rounded-full">
+            Download Resume
+          </span>
         </button>
       </div>
       <div className="col-span-5 place-self-center mt-4 lg:mt-0">
