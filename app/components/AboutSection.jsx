2fba301f6344696b44dfea2c3a19ace88866a63b
Raya Farhadi <raya.farhadi@gmail.com>
2024-03-01 19:51:29 -0500

    added smaller screen support

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const AboutSection = () => {
   return (
     <div className="text-white">
-      <div className="grid grid-cols-2 gap-16 py-16 px-16 items-start">
+      <div className="md:grid md:grid-cols-2 gap-8 xl:gap-16 py-8 px-4 sm:py-16 xl:px-16 items-center">
         <div className="px-1 py-1 w-full sm:w-fit rounded-md mt-3 sm:mt-0 bg-gradient-to-br theme-gradient">
           <Image
             className="rounded-md"
@@ -14,9 +14,9 @@ const AboutSection = () => {
             height={500}
           />
         </div>
-        <div className="text-left flex flex-col h-full">
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold mb-4">About Me</h2>
-          <p className="text-lg">
+          <p className="text-base lg:text-lg">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc sed
             augue lacus viverra vitae congue eu consequat. Feugiat in ante metus
