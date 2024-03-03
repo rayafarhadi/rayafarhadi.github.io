583f7be5fb813e232d5a66ea16c4a312aacc9ebe
Raya Farhadi <raya.farhadi@gmail.com>
2024-03-03 00:29:29 -0500

    added basic layout for projects section

---
+++ app/components/projects/ProjectSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ProjectSection = () => {
+  return (
+    <div className="text-white">
+      <h2>My Projects</h2>
+      <div>
+        <div>Project 1</div>
+        <div>Project 2</div>
+        <div>Project 3</div>
+        <div>Project 4</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSection;
