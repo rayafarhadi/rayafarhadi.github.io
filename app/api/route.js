2b1461c5d9ef0daa37aff361c181f92b369386e2
Raya Farhadi <raya.farhadi@gmail.com>
2024-03-08 23:59:43 -0500

    implemented contact form functionality with mailjet

--- app/api/route.js
+++ app/api/route.js
@@ -1,7 +1,43 @@
 import { NextResponse } from "next/server";
+import Mailjet from "node-mailjet";
 
 export async function POST(req, res) {
-  console.log("data", await req.json());
+  const { email, subject, message } = await req.json();
+
+  const mailjet = new Mailjet({
+    apiKey: process.env.MJ_APIKEY_PUBLIC,
+    apiSecret: process.env.MJ_APIKEY_PRIVATE,
+  });
+
+  await mailjet
+    .post("send", { version: "v3.1" })
+    .request({
+      Messages: [
+        {
+          From: {
+            Email: "raya.farhadi@gmail.com",
+            Name: "Contact Form",
+          },
+          To: [
+            {
+              Email: "raya.farhadi@gmail.com",
+              Name: "Raya Farhadi",
+            },
+          ],
+          Subject: subject,
+          TextPart: `New contact form entry from: ${email}.
+        
+        ${message}
+        `,
+        },
+      ],
+    })
+    .then((result) => {
+      console.log(result.body);
+    })
+    .catch((err) => {
+      console.log(err.statusCode);
+    });
 
   return NextResponse.json({ message: "OK" }, { status: 200 });
 }
