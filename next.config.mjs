0602374c901aff8ccd42cd07895d7c6ddf4540ed
Raya Farhadi <raya.farhadi@gmail.com>
2024-05-15 16:22:12 -0400

    Revert "updated next config"
    This reverts commit ae6d34cabd2903984936112454c41a9b39a565b7.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/Portfolio-Website",
-  images: {
-    unoptimized: true
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
